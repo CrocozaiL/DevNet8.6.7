6cf764b9fcde359a7c198fbc98a5145f20d84c3d
Zailani Abdillah <128051065+CrocozaiL@users.noreply.github.com>
2023-07-27 01:30:33 +0800

    Create llist-membership.py

---
+++ llist-membership.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+access_token = 'ZTEzMTNhYzAtODQ4Zi00Mzc2LTg3NWUtNjM4ZmE2ZjQ0NjEzMWU3ZThhZTAtYzRm_P0A1_c9a3c616-b39c-460f-b134-dd9f31b56890'
+room_id = 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vZmY2OGQxNTAtMmJkMi0xMWVlLTkzYjMtYjkwYjJmZDNhOGQ3'
+url = 'https://webexapis.com/v1/memberships'
+headers = {
+ 'Authorization': 'Bearer {}'.format(access_token),
+ 'Content-Type': 'application/json'
+}
+params = {'roomId': room_id}
+res = requests.get(url, headers=headers, params=params)
+print(json.dumps(res.json(), indent=4))
